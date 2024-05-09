d93e62bfc4dcd0127fcbc1ed1e55e5aaaf3001db
Sureshyadav2097 <sureshyadav4032@gmail.com>
2024-05-09 15:56:11 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Team from "./components/Team";
+import India from "./components/India";
 function App() {
   return (
-    <div className="APP">
-      <h1>Royal Challengers Bangalore</h1> 
-      <img className="logo" src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/RCB/Logos/Logooutline/RCBoutline.png"></img>
-      <p>
-          Royal Challengers Bangalore (often abbreviated as RCB) are a
-          professional franchise cricket team based in Bengaluru, Karnataka,
-          that plays in the Indian Premier League (IPL). It was founded in 2008
-          by United Spirits and named after the company's liquor brand Royal
-          Challenge. The Royal Challengers have not won the IPL but finished
-          runner-up on three occasions between 2009 and 2016. The team holds the
-          records of both the highest and the lowest totals in the IPL – 263/5
-          and 49 respectively.
-      </p>
-      <h2>Batters</h2>
-      <div className="Batters">
-      <img src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/2.png"></img>
-      <h3>VIRAT KOHLI</h3>
-      <img src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/94.png"></img>
-      <h3>FAF DU PLESIS</h3>
-      <img src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/597.png"></img>
-      <h3>RAJAT PATIDAR</h3>
-      <img src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/13.png"></img>
-      <h3>DINESH KARTHIK</h3>
-      <img src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/1941.png"></img>
-      <h3>WILL JACKS</h3>
-      </div>
-      <h2>All Rounders</h2>
-      <div className="Allrounders">
-      <img src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/28.png"></img>
-      <h3>GLEEN MAXWELL</h3>
-      <img src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/550.png"></img>
-      <h3>CAMERON GREEN</h3>
-      </div>
-      <h2>Bowlers</h2>
-      <div className="Bowlers">
-      <img src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/63.png"></img>
-      <h3>MOHAMMED SIRAJ</h3>
-      <img src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/978.png"></img>
-      <h3>YASH DAYAL</h3>
-      <img src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/69.png"></img>
-      <h3>LOKIE FURGUSON</h3>
-      </div>
-    </div>
+   <div>
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<Team></Team>}></Route>
+  <Route path="/india" element={<India></India>}></Route>
+</Routes>
+</BrowserRouter>
+
+   </div>
+
+
+
+
+
   );
 }
 
